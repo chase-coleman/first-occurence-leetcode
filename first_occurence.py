847d80e230b221d8123fa178776f9efd81b578cc
YOUR_NAME <YOUR_EMAIL>
2025-02-25 19:07:58 -0600

    Completed First occurence challenge.

---
+++ first_occurence.py
@@ -0,0 +1,24 @@
+
+# Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+# Example 1:
+
+# Input: haystack = "sadbutsad", needle = "sad"
+# Output: 0
+# Explanation: "sad" occurs at index 0 and 6.
+# The first occurrence is at index 0, so we return 0.
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        if needle in haystack:
+            return haystack.index(needle)
+        else:
+            return -1
+        
+        
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+test = Solution()
+test.strStr("sadbutsad", "sad")
